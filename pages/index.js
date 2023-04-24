86e37ea96d1bf6eb990c3b616fa36f4c52d38278
sivasuriyan-p <97089865+sivasuriyan-p@users.noreply.github.com>
2023-04-24 18:05:22 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 export default function TetsTwo() {
   return (
     <div>
-      <div style={{ color: "red" }}>
+      <div style={{ color: "green" }} className="test">
         <div >
           <br />
           <br />
           <p >The example test</p>
+          <p >The example test</p>
+
         </div>
       </div>
     </div>
