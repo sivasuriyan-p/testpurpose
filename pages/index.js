a009d5e44b1f557edc8de2eb1283bf9cb47a937b
sivasuriyan-p <sivasuriyan58@gmail.com>
2023-04-24 17:41:17 +0530

    test

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+export default function TetsTwo() {
+  return (
+    <div>
+      <div style={{ color: "red" }}>
+        <div >
+          <br />
+          <br />
+          <p >The example test</p>
+        </div>
+      </div>
+    </div>
+  )
+}
